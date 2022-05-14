7ae67737985c922510cac37d1fd201c6e8643620
V3il3DOraCl3 <javadaaghajanloo@gmail.com>
2022-05-14 11:05:25 +0330

    BaseOfOOP

---
+++ classes_&_oop_training/Base.Of.OOP.py
@@ -0,0 +1,9 @@
+from Bicycle import Bike
+from Bicycle import MTB
+from Bicycle import Road
+Bikee = Bike('Blue', 'road')
+Trek = MTB('Trek', 29, 18.5)
+Koga = Road('Carbon', 'B&W', 'RoadBicycle')
+Trek.Bike_Info()
+Trek.printH()
+Koga.printRoad()
--- classes_&_oop_training/Bicycle.py
+++ classes_&_oop_training/Bicycle.py
@@ -1,16 +1,29 @@
 class Bike():
     def __init__(self, Color, Type):
+        print('A bike object is being created!')
         self.Color = Color
         self.Type = Type
+
+    def printH(self):
+        print('hello')
 
 
 class MTB(Bike):
     def __init__(self, name, WS, FS):
+        super().__init__("red", 'yo')
         self.name = name
         self.WS = WS
         self.FS = FS
 
     def Bike_Info(self):
         print(self.name + ' - ' + str(self.WS) +
-              ' - ' + str(self.FS) + ' - ' + Bike.Color)
+              ' - ' + str(self.FS) + ' - ' + self.Color)
 
+
+class Road(Bike):
+    def __init__(self, Material, Color, Type):
+        super().__init__(Color, Type)
+        self.Material = Material
+
+    def printRoad(self):
+        print('this ' + self.Material + ' bicycle for race and road training ' + 'its color is ' + self.Color)
